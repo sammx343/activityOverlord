1377854012b26f371696298e065d30890e2b317e
Sebastian Mayor <naitsabes29@gmail.com>
2017-01-16 22:22:09 -0500

    first commit

---
+++ api/controllers/UserController.js
@@ -0,0 +1,50 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+	'new': function(req, res){
+    res.view();
+  },
+
+  create: function (req, res, next){
+    User.create( req.params.all(), function userCreated (err,user){
+
+      if(err){
+        //console.log(err);
+        req.session.flash = {
+          err : err
+        }
+
+        return res.redirect('/user/new');
+      }
+
+      res.redirect('/user/show/'+ user.id)
+    });
+  },
+
+  show: function(req, res, next){
+    User.findOne(req.param('id'), function foundUser (err, user){
+      if (err) return next(err);
+      if (!user) return next();
+      res.view({
+        user : user
+      });
+    });
+  },
+
+  index: function(req, res, next){
+    User.find(function foundUsers (err, users){
+      if (err) return next(err);
+
+      res.view({
+        users : users
+      });
+    });
+  }
+};
+
